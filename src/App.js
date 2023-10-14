44f868104df424d120ace4b345ab38be608251fe
Mohit Jain <dalemohit9@gmail.com>
2023-10-15 00:35:18 +0530

    adding some functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
    <>
-   how are you?
+      <Nav/>
+      <Header/>
+      <Main/>
+      <Footer/>
    </>
   );
 }
 
 export default App;
+
---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+function Nav(){
+    return (
+        <nav>
+            <img src="./logo.jpg" alt="little lemon" width="100px"/>
+            <ul type="none">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Reservations</a></li>
+                <li><a href="#">Order online</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
+
+
+
+
+
+
+
+
+
