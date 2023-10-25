91a6b7f724b6be09ec0a794b7e9597bb8a65509b
Mohit Jain <dalemohit9@gmail.com>
2023-10-25 23:13:55 +0530

    finishing responsive design

--- src/CallToAction.js
+++ src/CallToAction.js
@@ -2,14 +2,16 @@ import {  Link } from "react-router-dom";
 
 export default function CallToAction(){
     return (
+        <div className="contain">
         <div className="hero container">
-            <div className="content">
+            {/* <div className="content"> */}
                 <h1>Little Lemon</h1>
                 <h2>Chicago</h2>
                 <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
                 <Link to="/booking" ><button className="button">Reserve Table</button></Link>
-            </div>
+            {/* </div> */}
             <img src="assets/header.jpg" alt="restuarant"/>
+        </div>
         </div>
     );
 }
